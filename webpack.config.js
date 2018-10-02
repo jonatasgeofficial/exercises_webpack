ed7091815160ba686eb6f401d17ee799db271a06
jonatasgeofficial <jonatasge@gmail.com>
2018-10-01 23:31:57 -0300

    Changes to settings and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 
 module.exports = {
+    mode: 'development',
     entry: './ex/index.js',
     output: {
         path: __dirname + '/public',
@@ -9,5 +10,11 @@ module.exports = {
     devServer: {
         port: 8080,
         contentBase: './public'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: ['babel-loader']
+        }]
     }
 }
